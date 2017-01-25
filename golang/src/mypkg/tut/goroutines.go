a31b2f660167f08bf000465ff8b1fcfefbcb1d48
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-26 03:42:51 +0900

    Life is not complex. We are complex. Life is simple, and the simple thing is the right thing. -- Oscar Wilde

--- golang/src/mypkg/tut/goroutines.go
+++ golang/src/mypkg/tut/goroutines.go
@@ -1,4 +1,4 @@
-//Time-stamp: <2017-01-26 03:18:29 hamada>
+//Time-stamp: <2017-01-26 03:42:28 hamada>
 package tut
 
 import (
@@ -6,21 +6,29 @@ import (
 	"time"
 )
 
-func say(s string) {
-	time.Sleep(10 * time.Millisecond)
-	fmt.Println(s)
-}
-
-func say5(s string) {
+func say(s string, c chan int) {
 	time.Sleep(1000 * time.Millisecond)
 	fmt.Println(s)
+	c <- 1
+}
+
+func say5(s string, c chan int) {
+	time.Sleep(999 * time.Millisecond)
+	fmt.Println(s)
+	c <- 5
 }
 
 func Goroutines() {
+
+	c := make(chan int)
+
 	for i := 0; i < 100; i++ {
-		go say(fmt.Sprintf("%v: -X-", i))
-		go say(fmt.Sprintf("%v: O", i))
+		go say(fmt.Sprintf("%v: oops", i), c)
+		go say(fmt.Sprintf("%v: ^_^;", i), c)
+		go say5(fmt.Sprintf("%v: five!", i), c)
+		x, y := <-c, <-c
+		fmt.Println(x, y)
 	}
 
-	time.Sleep(10000 * time.Millisecond)	
+//	time.Sleep(10000 * time.Millisecond)	
 }
