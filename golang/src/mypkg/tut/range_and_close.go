1da58e99084e03ef0640a9127921d639ed473ecb
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-26 05:55:21 +0900

    Without haste, but without rest. -- Johann Wolfgang von Goethe

--- golang/src/mypkg/tut/range_and_close.go
+++ golang/src/mypkg/tut/range_and_close.go
@@ -1,4 +1,4 @@
-//Time-stamp: <2017-01-26 05:42:06 hamada>
+//Time-stamp: <2017-01-26 05:55:15 hamada>
 package tut
 
 import (
@@ -7,24 +7,19 @@ import (
 
 func fibonacci(n int, c chan int) {
 	x, y := 0, 1
-	for i := 0; i < 0xffffff; i++ {
+	for i := 0; i < n*5; i++ {
 		c <- x
-		//		x, y = y, x+y
-		x, y = y, y+1
+		x, y = y, x+y
 	}
 	close(c)
 }
 
 func Range_and_close() {
-	c := make(chan int, 1)
+	c := make(chan int, 5)
 	go fibonacci(cap(c), c)
 
-	sum := int(0)
 	for v := range c {
-		sum = sum + v
-//		fmt.Printf("%v, %v, %v, %v\n", v, len(c),cap(c), c)
+		fmt.Printf("%v, %v, %v, %v\n", v, len(c), cap(c), c)
 	}
-	fmt.Println(sum)
 
 }
-
