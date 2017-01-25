ceb0762e06ec02ea28e0f3d9384eee48ed12e353
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-26 01:51:43 +0900

    The sole meaning of life is to serve humanity. -- Leo Tolstoy

--- golang/src/mypkg/exer/rot_reader.go
+++ golang/src/mypkg/exer/rot_reader.go
@@ -1,4 +1,4 @@
-//Time-stamp: <2017-01-26 01:42:18 hamada>
+//Time-stamp: <2017-01-26 01:51:26 hamada>
 package exer
 
 import (
@@ -12,13 +12,13 @@ type rot13Reader struct {
 	r io.Reader
 }
 
-func rot13(c byte) byte {
+func nrot(c byte, n int) byte {
 	var crot byte
 	switch {
 	case ('A' <= c && c <= 'Z'):
-		crot = (c-'A'+13)%26 + 'A'
+		crot = (c-'A'+byte(n))%26 + 'A'
 	case ('a' <= c && c <= 'z'):
-		crot = (c-'a'+13)%26 + 'a'
+		crot = (c-'a'+byte(n))%26 + 'a'
 	default:
 		crot = c
 	}
@@ -41,19 +41,26 @@ func (rd rot13Reader) Read(buf []byte) (n int, err error) {
 	}
 
 	for i := range buf0 {
-		b := rot13(buf0[i])
+		b := nrot(buf0[i], 13)
 		buf[i] = b
 	}
 
 	return
 }
 
-func Rot_reader() {
-
-	//		s := strings.NewReader("Lbh penpxrq gur pbqr!")
-	s := strings.NewReader("Neg jnfurf njnl sebz gur fbhy gur qhfg bs rirelqnl yvsr. -- Cnoyb Cvpnffb")
+func do (msg string) {
+	s := strings.NewReader(msg)
 	r := rot13Reader{s}
 	io.Copy(os.Stdout, &r)
 	fmt.Printf("\n")
+}
+
+func Rot_reader() {
+
+	do("Lbh penpxrq gur pbqr!")
+	do("Neg jnfurf njnl sebz gur fbhy gur qhfg bs rirelqnl yvsr. -- Cnoyb Cvpnffb")
+	do("Vg qbrf abg znggre ubj fybjyl lbh tb fb ybat nf lbh qb abg fgbc. -- Pbhshpvhf")
+	do("Vs bar nqinaprf pbasvqragyl va gur qverpgvba bs uvf qernzf, naq raqrnibef gb yvir gur yvsr juvpu ur unf vzntvarq, ur jvyy zrrg jvgu n fhpprff harkcrpgrq va pbzzba ubhef. -- Urael Qnivq Gubernh")
+	do("Bhe terngrfg tybel vf abg va arire snyyvat, ohg va evfvat hc rirel gvzr jr snvy. -- Enycu Jnyqb Rzrefba")
 
 }
