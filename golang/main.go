6314fae5b12fdfba1195a15ae617d2e18fd5d93f
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-25 11:29:20 +0900

    The snake which cannot cast its skin has to die. As well the minds which are prevented from changing their opinions; they cease to be mind. -- Friedrich Nietzche

--- golang/main.go
+++ golang/main.go
@@ -1,25 +1,34 @@
-//Time-stamp: <2017-01-25 10:32:39 hamada>
+//Time-stamp: <2017-01-25 11:28:34 hamada>
 package main
 
 import (
 	"fmt"
-//	"golang.org/x/tour/pic"
+	"golang.org/x/tour/pic"
 	"mypkg1"
 )
 
-/*
 func Pic(dx, dy int) [][]uint8 {
-	z := [][]uint8
-	return z
+
+	p := make([][]uint8, 0)
+
+	for iy := 0; iy < dy; iy++ {
+		pline := make([]uint8, dx)
+		for ix := 0; ix < ix; ix++ {
+			pline[ix] = uint8(0xff & (ix+iy))
+		}
+		p = append(p, pline)
+	}
+
+	return p
 }
-*/
 
 func main() {
 
-//	pic.Show(Pic)
-	mypkg1.Main()
-	fmt.Println("Hello!!")
+	pic.Show(Pic)
+
 	if false {
 		mypkg1.Main()
+		fmt.Printf("%v, %T\n", mypkg1.Main, mypkg1.Main)
 	}
+
 }
