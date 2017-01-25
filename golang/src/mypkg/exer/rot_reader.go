409ca9b9407882b78d83bcbb4c098a923e9b6b4d
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-26 01:14:28 +0900

    It's not the years in your life that count. It's the life in your years. -- Abraham Lincoln

--- golang/src/mypkg/exer/rot_reader.go
+++ golang/src/mypkg/exer/rot_reader.go
@@ -1,24 +1,39 @@
-//Time-stamp: <2017-01-26 00:25:14 hamada>
+//Time-stamp: <2017-01-26 01:13:12 hamada>
 package exer
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"strings"
-	"fmt"
 )
 
 type rot13Reader struct {
 	r io.Reader
 }
 
-func (rd rot13Reader) Read([]byte) (int, error) {
-	fmt.Printf("%v\n", rd.r)
-	return 0, nil
+func (rd rot13Reader) Read(buf []byte) (n int, err error) {
+	var DEBUG bool = true //false
+
+	buf0 := make([]byte, 10)
+	n, err = rd.r.Read(buf0)
+	if DEBUG {
+		fmt.Printf("%v, %v\n", n, err)
+		fmt.Printf("%v\n", buf0)
+	}
+	for i := 0; i < n; i++ {
+		buf[i] = (((buf0[i] - 97) + 13) % 26) + 97
+	}
+	return
 }
 
 func Rot_reader() {
-	s := strings.NewReader("Lbh penpxrq gur pbqr!")
-	r := rot13Reader{s}
-	io.Copy(os.Stdout, &r)
+
+	if true {
+//		s := strings.NewReader("Lbh penpxrq gur pbqr!")
+//		s := strings.NewReader("a b c d e f g")
+		s := strings.NewReader("Lbhpenpxrqgurpbqr!")
+		r := rot13Reader{s}
+		io.Copy(os.Stdout, &r)
+	}
 }
