5871124094d2252f29274746c55899a324522187
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-27 00:24:39 +0900

    Man lives freely only by his readiness to die. -- Mahatma Gandhi

--- golang/src/mypkg/tut/select.go
+++ golang/src/mypkg/tut/select.go
@@ -1,18 +1,21 @@
-//Time-stamp: <2017-01-27 00:07:18 hamada>
+//Time-stamp: <2017-01-27 00:24:13 hamada>
 package tut
 
 import (
 	"fmt"
+	"time"
 )
 
-func fibon(c, quit chan int) {
+func fibon(c, quit, exit chan int) {
 	x, y := 0, 1
 	for {
 		select {
-		case c <- x:
+		case c <- x: // --- send to c ---
 			x, y = y, x+y
-		case <-quit:
+			time.Sleep(500 * time.Millisecond)
+		case <-quit: // --- recv from quit ---
 			fmt.Println("quit")
+			exit <- 0
 			return
 		}
 	}
@@ -28,6 +31,7 @@ func print(c, quit chan int) {
 func Select() {
 	c := make(chan int)
 	quit := make(chan int)
+	exit := make(chan int)
 	if true {
 		go func() {
 			for i := 0; i < 10; i++ {
@@ -38,5 +42,9 @@ func Select() {
 	} else {
 		go print(c, quit)
 	}
-	fibon(c, quit)
+
+	go fibon(c, quit, exit)
+
+	<-exit // sync thread fibon
+
 }
