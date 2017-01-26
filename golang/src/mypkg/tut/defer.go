21c31e22b22696a8ac29e6b708b4e89de8eee3e1
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-27 03:31:30 +0900

    You don't need any brains to listen to music. -- Luciano Pavarotti

--- golang/src/mypkg/tut/defer.go
+++ golang/src/mypkg/tut/defer.go
@@ -1,25 +1,29 @@
-//Time-stamp: <2017-01-27 03:24:49 hamada>
+//Time-stamp: <2017-01-27 03:31:18 hamada>
 package tut
 
 import (
 	"fmt"
 )
 
-func sub_201701270317_b(s string) string {
-	fmt.Println("sub_b:", s)
-	return s
-}
-
-func sub_201701270317_a(x int) string {
-	s := fmt.Sprintf("hello %v", x)
-	defer fmt.Println("sub_a: Defer!")
-	return sub_201701270317_b(s)
-}
 
 func Defer() {
 
+	var func_a func (x int) string
+	var func_b func (s string) string 
+
+	func_b = 	func (s string) string {
+		fmt.Println("sub_b:", s)
+		return s
+	}
+
+	func_a = func (x int) string {
+		s := fmt.Sprintf("hello %v", x)
+		defer fmt.Println("sub_a: Defer!")
+		return func_b(s)
+	}
+
 	for i := 0; i < 10; i++ {
-		fmt.Println("main:", sub_201701270317_a(i))
+		fmt.Println("main:", func_a(i))
 		fmt.Println()
 	}
 
