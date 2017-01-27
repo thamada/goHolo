8ae17859e950ad8c4c676e3266ec01260fff2e52
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-28 00:31:18 +0900

    The greatest mistake you can make in life is be continually fearing you will make one. -- Elbert Hubbard

--- golang/src/mypkg/tut/mutex_counter.go
+++ golang/src/mypkg/tut/mutex_counter.go
@@ -1,4 +1,4 @@
-//Time-stamp: <2017-01-28 00:15:56 hamada>
+//Time-stamp: <2017-01-28 00:30:18 hamada>
 package tut
 
 import (
@@ -10,29 +10,36 @@ import (
 type SafeCounter struct {
 	v   map[string]int
 	mux sync.Mutex
+	seq [15]int
 }
 
-func (c *SafeCounter) Inc(key string) {
+func (c *SafeCounter) Write(key string, i int) {
 	c.mux.Lock()
 	c.v[key]++
+	fmt.Println(i, ": incr: ", c.v[key])
+	c.seq[i] = c.v[key]
 	defer c.mux.Unlock()
 }
 
-func (c *SafeCounter) Value(key string) int {
+func (c *SafeCounter) Read(key string, i int) int {
 	c.mux.Lock()
-	fmt.Println(c.Value("somekey"))
+	fmt.Println(i, ": read: ", c.v[key])
 	defer c.mux.Unlock()
 	return c.v[key]
 }
 
 func Mutex_counter() {
 	c := SafeCounter{v: make(map[string]int)}
+
 	for i := 0; i < 10; i++ {
-		go c.Value("somekey")
-		go c.Inc("somekey")
+		go c.Read("mutex", i)
+		go c.Write("mutex", i)
+		go c.Write("mutex", i)
 	}
 
-	fmt.Println(c.Value("somekey"))
+	fmt.Println(c.Read("mutex", -1))
 	time.Sleep(time.Second)
+	fmt.Println(c.Read("mutex", -2))
+	fmt.Printf("seq: %v\n", c.seq)
 }
 
