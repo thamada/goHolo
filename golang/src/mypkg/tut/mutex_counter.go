e8b0c80eef1a4d9172e8b09329161a8c9e11aeaa
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-28 00:37:26 +0900

    To live is to think. -- Cicero

--- golang/src/mypkg/tut/mutex_counter.go
+++ golang/src/mypkg/tut/mutex_counter.go
@@ -1,4 +1,4 @@
-//Time-stamp: <2017-01-28 00:30:18 hamada>
+//Time-stamp: <2017-01-28 00:36:19 hamada>
 package tut
 
 import (
@@ -10,14 +10,14 @@ import (
 type SafeCounter struct {
 	v   map[string]int
 	mux sync.Mutex
-	seq [15]int
+	seq []int
 }
 
 func (c *SafeCounter) Write(key string, i int) {
 	c.mux.Lock()
 	c.v[key]++
 	fmt.Println(i, ": incr: ", c.v[key])
-	c.seq[i] = c.v[key]
+	c.seq = append(c.seq, i)
 	defer c.mux.Unlock()
 }
 
@@ -30,11 +30,14 @@ func (c *SafeCounter) Read(key string, i int) int {
 
 func Mutex_counter() {
 	c := SafeCounter{v: make(map[string]int)}
+	c.seq = make([]int, 0)
 
 	for i := 0; i < 10; i++ {
+		go c.Write("mutex", i+0)
+		go c.Write("mutex", i+100)
 		go c.Read("mutex", i)
-		go c.Write("mutex", i)
-		go c.Write("mutex", i)
+		go c.Write("mutex", i+200)
+		go c.Write("mutex", i+300)
 	}
 
 	fmt.Println(c.Read("mutex", -1))
