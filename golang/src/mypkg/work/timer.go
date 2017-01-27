9e855f6efc73a0d08b4d1b137b07c9ecf15bc008
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-28 04:14:42 +0900

    What do you want meaning for ? Life is desire, not meaning. -- Charlie Chaplin

--- golang/src/mypkg/work/timer.go
+++ golang/src/mypkg/work/timer.go
@@ -1,32 +1,27 @@
-//Time-stamp: <2017-01-28 04:07:48 hamada>
+//Time-stamp: <2017-01-28 04:14:09 hamada>
 package work
 
 import "fmt"
 
 import "time"
 
-func timer(id int, d time.Duration) <-chan int {
-	c := make(chan int)
-
-	if true {
-		go func() {
-			time.Sleep(d)
-			c <- id
-		}()
-	} else {
-		time.Sleep(d)
-		c <- id
-	}
-
-	return c
+func timer(id int, d time.Duration, ch chan int) {
+	time.Sleep(d)
+	ch <- id
 }
 
 func Timer() {
+	ni := 100 
+	c := make(chan int, ni)
 
-	for i := 0; i < 24; i++ {
-		c := timer(i, 100 * time.Millisecond)
-		fmt.Printf("[%v]: ", i)
-		fmt.Printf("%v\n", <-c)
-
+	for i := 0; i < ni; i++ {
+		t := 5000 * time.Millisecond
+		go timer(i, t, c)
+		fmt.Printf("done %v\n", i)
 	}
+
+	for i := 0; i < ni; i++ {
+		fmt.Printf("%v\n", <-c)
+	}
+
 }
