facf6e52f27a89d0ee4e18ebbf7fa7f8f00c1d1e
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-29 03:08:24 +0900

    We don't stop playing because we grow old; we grow old because we stop playing. -- George Bernard Shaw

--- golang/src/mypkg/work/sync.go
+++ golang/src/mypkg/work/sync.go
@@ -1,23 +1,32 @@
-//Time-stamp: <2017-01-29 03:05:19 hamada>
+//Time-stamp: <2017-01-29 03:08:01 hamada>
 package work
 
 import (
 	log "fmt"
-	"time"
 	"sync"
+	"time"
 )
 
 func Sync() {
 	var wg sync.WaitGroup
 	for i := 0; i < 100; i++ {
 		wg.Add(1)
-		go func(i int) {
+		/*
+			go func(i int) {
+				time.Sleep(1500 * time.Millisecond)
+				id := i
+				log.Printf("%03d\n", id)
+				//			log.Println("finish", id)
+				wg.Done()
+			}(i)
+		*/
+		go func() {
 			time.Sleep(1500 * time.Millisecond)
-			id := i
-			log.Printf("%03d\n", id)
-//			log.Println("finish", id)
+			log.Printf("%03d\n", i)
+			//			log.Println("finish", id)
 			wg.Done()
-		}(i)
+		}()
+
 	}
 	wg.Wait()
 }
