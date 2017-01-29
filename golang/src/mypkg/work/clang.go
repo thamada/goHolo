9adac63770a5b1b94035564020939f9659be79f9
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-30 03:13:07 +0900

    If you want to be happy, be. -- Leo Tolstoy

--- golang/src/mypkg/work/clang.go
+++ golang/src/mypkg/work/clang.go
@@ -1,4 +1,4 @@
-//Time-stamp: <2017-01-30 03:11:19 hamada>
+//Time-stamp: <2017-01-30 03:12:46 hamada>
 package work
 
 /*
@@ -8,11 +8,13 @@ package work
 import "C"
 
 import (
+	"fmt"
 	"unsafe"
 )
 
 func Clang() {
 	p := C.CString("Hello from C language!")
+	fmt.Printf("%v, %T\n", p, p)
 	C.puts(p)
 	C.free(unsafe.Pointer(p))
 }
