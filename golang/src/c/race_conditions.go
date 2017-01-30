36d9cd9d7f5b60e84d6d3ea9a1264580ab398e57
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-31 00:12:31 +0900

    In order to write about life, first you must live it! -- Ernest Hemingway

--- golang/src/c/race_conditions.go
+++ golang/src/c/race_conditions.go
@@ -1,37 +1,12 @@
-//Time-stamp: <2017-01-30 04:33:17 hamada>
+//Time-stamp: <2017-01-31 00:08:29 hamada>
 package main
 
 import (
 	"fmt"
 	"time"
+	"../mypkg/work/race_conditions.go"
 )
 
-func Race_conditions() {
-	ni := 100
-
-	for i := 0; i < ni; i++ {
-
-		// calc() is a closure which has 'i' on the shared memory.
-		calc := func(id int) {
-			ii := i // i is on shared memory
-			time.Sleep(100 * time.Nanosecond)
-			var s = fmt.Sprintf("%v%v", id, i)
-			if ii != i {
-				fmt.Printf("id=%v, i=%v\n", id, i)
-				fmt.Println(s)
-				fmt.Println([]byte(s))
-			}
-		}
-
-		if true {
-			go calc(i)
-		} else {
-			calc(i)
-		}
-		time.Sleep(10000 * time.Nanosecond)
-	}
-	time.Sleep(10 * time.Millisecond)
-}
 
 func main() {
 	Race_conditions()
